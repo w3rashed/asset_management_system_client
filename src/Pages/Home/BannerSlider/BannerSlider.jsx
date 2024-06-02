5e65488ec2b47663e8fc2f853e852adcbadc2e60
Rashed <w3rashed@gmail.com>
2024-06-02 10:24:12 +0600

    home bannner slider added

--- src/Pages/Home/BannerSlider/BannerSlider.jsx
+++ src/Pages/Home/BannerSlider/BannerSlider.jsx
@@ -1,5 +1,28 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 const BannerSlider = () => {
-  return <div>slider</div>;
+  return (
+    <Swiper
+      pagination={true}
+      modules={[Pagination]}
+      className="mySwiper w-full"
+    >
+      <SwiperSlide>
+        <img src="https://i.ibb.co/vd4rBnr/bg.jpg" alt="" className="w-full" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://i.ibb.co/vd4rBnr/bg.jpg" alt="" className="w-full" />
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 export default BannerSlider;
