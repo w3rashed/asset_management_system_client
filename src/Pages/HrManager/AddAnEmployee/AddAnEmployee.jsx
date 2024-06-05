d0c1086adbc4acebdfda5187e5e5ae8cae5ee68e
Rashed <w3rashed@gmail.com>
2024-06-06 02:12:35 +0600

    add an employee in data base done

--- src/Pages/HrManager/AddAnEmployee/AddAnEmployee.jsx
+++ src/Pages/HrManager/AddAnEmployee/AddAnEmployee.jsx
@@ -1,14 +1,47 @@
 import useAllEmployee from "@/Hooks/useAllEmployee";
+import useAxiosPublic from "@/Hooks/useAxiosPublic";
+import useUsers from "@/Hooks/useUsers";
 
 import SectionTitle from "@/components/SectionTitle/SectionTitle";
 import { Button } from "@/components/ui/button";
+import Swal from "sweetalert2";
 
 const AddAnEmployee = () => {
   const [allEmployee, refetch] = useAllEmployee();
+  const axiosPublic = useAxiosPublic();
+  const [data] = useUsers();
   console.log(allEmployee, "all employee");
 
   const handleAdd = (employee) => {
     console.log(employee, "handle add");
+    const teamInfo = {
+      hrEmail: data.email,
+      company_logo: data.company_logo,
+      employee_email: employee.email,
+    };
+    console.log(teamInfo, "team info");
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic.post("/my_employee", teamInfo).then((res) => {
+          console.log(res.data);
+          if (res.data.insertedId) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
   };
   return (
     <div>
