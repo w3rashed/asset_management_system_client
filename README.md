0bb0b140c94db8968dde8b02d511727d9f664338
Rashed <w3rashed@gmail.com>
2024-06-15 17:17:28 +0600

    asstet list redesign table add search filter and sort

--- README.md
+++ README.md
@@ -6,27 +6,17 @@
 
 **ASSET NEX**
 - **Home pag**: Users can see an overview of our mission.
-- **Need Volunteer Posts**: Browse posts listing volunteer opportunities.
-- **Search**: Search for posts by title.
-- **Post Details**: Detailed information on volunteer opportunities, including requirements and location.
-- **Request a Volunteer**:Submit requests for volunteers for your projects or events.
-- **Add a post for Volunteer**:Create posts to recruit volunteers for specific tasks or causes.
-- **My posts**:  View, edit, or delete posts you've added.
-- **MyRequests**:  View and manage requests you've made to volunteer for opportunities.
-- 
+
 
 
 # NPM pacages .....
 - [React Hook from](https://react-hook-form.com)
-- [React Fast Marquee](https://www.react-fast-marquee.com)
-- [React datepicker](https://reactdatepicker.com)
 - [React icons](https://react-icons.github.io/react-icons)
 - [React sweetalert2](https://sweetalert2.github.io)
-- [react-simple-typewriter](https://github.com/awran5/react-simple-typewriter)
 
 
 
 
 # GitHub Private repo links
-- [Client repo](https://github.com/Porgramming-Hero-web-course/b9a11-client-side-w3rashed.git)
-- [server repo](https://github.com/Porgramming-Hero-web-course/b9a11-server-side-w3rashed.git)
+- [Client repo](https://github.com/programming-hero-web-course1/b9a12-client-side-w3rashed.git)
+- [server repo](https://github.com/programming-hero-web-course1/b9a12-server-side-w3rashed.git)
--- src/Pages/HrManager/AssetsList/AssetsList.jsx
+++ src/Pages/HrManager/AssetsList/AssetsList.jsx
@@ -1,74 +1,78 @@
-import useAssetsList from "@/Hooks/useAssetsList";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
 import { Button } from "@/components/ui/button";
-import {
-  Paper,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-} from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { format } from "date-fns";
-import { Table } from "lucide-react";
-import React from "react";
-import { Helmet } from "react-helmet";
+import useAxiosPublic from "@/Hooks/useAxiosPublic";
+import useAuth from "@/Hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import { IoMdSearch } from "react-icons/io";
 
 const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
-  {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value) => value.toFixed(2),
-  },
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767),
+  { id: "Product Name", label: "Product Name", minWidth: 170 },
+  { id: "Product Type", label: "Product Type", minWidth: 170 },
+  { id: "Product Quantity", label: "Product Quantity", minWidth: 170 },
+  { id: "Added Date", label: "Added Date", minWidth: 170 },
+  { id: "Action", label: "Action", minWidth: 170 },
 ];
 
 export default function AssetsList() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  // const [assets_list, refetch] = useAssetsList();
+  const [searchValue, setSearchValue] = React.useState();
+  const [filterValue, setFilterValue] = React.useState();
+  const [sortValue, setSortValue] = React.useState();
+
+  // search
+
+  const handleSearch = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  // filter
+  const handleFilter = (e) => {
+    setFilterValue(e.target.value);
+  };
+
+  // sort
+  const handleSort = (e) => {
+    setSortValue(e.target.value);
+  };
+
+  const axiosPublic = useAxiosPublic();
+  const { user } = useAuth();
+  const { data: assets_list = [], refetch } = useQuery({
+    queryKey: ["assets_lists", user?.email],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/assets/${user?.email}`, {
+        params: {
+          searchValue,
+          filterValue,
+          sortValue,
+        },
+      });
+      return res.data;
+    },
+  });
+
+  React.useEffect(() => {
+    if (filterValue !== null || sortValue !== null || searchValue !== null) {
+      refetch();
+    }
+  }, [refetch, filterValue, sortValue, searchValue]);
+
+  console.log(searchValue, filterValue, sortValue);
+
+  const handleUpdate = (row) => {
+    console.log(row);
+  };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -81,27 +85,47 @@ export default function AssetsList() {
 
   return (
     <div>
-      <Helmet>
-        <title>Asset Nex | Asset List</title>
-      </Helmet>
+      <div className="flex justify-center mt-2 mb-4">
+        <label className="input input-bordered flex items-center gap-2">
+          <input
+            onChange={handleSearch}
+            value={searchValue}
+            type="text"
+            className="grow"
+            placeholder="Search"
+          />
+          <IoMdSearch></IoMdSearch>
+        </label>
+        <div>
+          <select onChange={handleFilter} name="" id="">
+            <option value="" disabled selected>
+              Select Filter
+            </option>
+            <option value="available">Available</option>
+            <option value="out_of_stock">Out of Stock</option>
+            <option value="returnable">Returnable</option>
+            <option value="non_returnable">Non Returnable</option>
+          </select>
+        </div>
+        <div>
+          <select onChange={handleSort} name="" id="">
+            <option value="" disabled selected>
+              Select Sort
+            </option>
+            <option value="Quantity">Quantity</option>
+          </select>
+        </div>
+      </div>
       <Paper sx={{ width: "100%" }}>
         <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
             <TableHead>
               <TableRow>
-                <TableCell align="center" colSpan={2}>
-                  Country
-                </TableCell>
-                <TableCell align="center" colSpan={3}>
-                  Details
-                </TableCell>
-              </TableRow>
-              <TableRow>
                 {columns.map((column) => (
                   <TableCell
                     key={column.id}
                     align={column.align}
-                    style={{ top: 57, minWidth: column.minWidth }}
+                    style={{ minWidth: column.minWidth }}
                   >
                     {column.label}
                   </TableCell>
@@ -109,7 +133,7 @@ export default function AssetsList() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows
+              {assets_list
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row) => {
                   return (
@@ -119,16 +143,25 @@ export default function AssetsList() {
                       tabIndex={-1}
                       key={row.code}
                     >
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                          <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === "number"
-                              ? column.format(value)
-                              : value}
-                          </TableCell>
-                        );
-                      })}
+                      {/* {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })} */}
+                      <TableCell>{row?.product_name}</TableCell>
+                      <TableCell>{row?.product_type}</TableCell>
+                      <TableCell>{row?.product_quantity}</TableCell>
+                      <TableCell>{row?.added_date.slice(0, 10)}</TableCell>
+                      <TableCell>
+                        <Button onClick={() => handleUpdate(row)}>
+                          hiiiiii
+                        </Button>
+                      </TableCell>
                     </TableRow>
                   );
                 })}
@@ -136,9 +169,9 @@ export default function AssetsList() {
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
+          rowsPerPageOptions={[5, 10]}
           component="div"
-          count={rows.length}
+          count={assets_list.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
@@ -148,67 +181,3 @@ export default function AssetsList() {
     </div>
   );
 }
-
-// const AssetsList = () => {
-
-//   const [assets_list, refetch] = useAssetsList();
-//   console.log(assets_list);
-
-//   const columns = [
-//     { field: "id", headerName: "#", width: 70 },
-//     { field: "name", headerName: "Product Name", width: 130 },
-//     { field: "type", headerName: "Product Type", width: 130 },
-//     { field: "quantity", headerName: "Product Quantity", width: 130 },
-//     { field: "date", headerName: "Date Added", width: 130 },
-
-//     {
-//       field: "actions",
-//       headerName: "Actions",
-//       width: 200,
-//       renderCell: (params) => (
-//         <div className="flex items-center gap-2">
-//           <Button onClick={() => handleUpdate(params.row.id)}>Update</Button>
-//           <Button onClick={() => handleDelete(params.row.id)}>Delete</Button>
-//         </div>
-//       ),
-//     },
-//   ];
-
-//   const rows = assets_list.map((asset, index) => {
-//     const date = new Date(asset.added_date);
-//     return {
-//       id: index + 1,
-//       name: asset.product_name,
-//       type: asset.product_type,
-//       quantity: asset.product_quantity,
-//       date: date.toLocaleDateString("en-GB"),
-//     };
-//   });
-
-//   const handleUpdate = (id) => {
-//     console.log(`Update row with id ${id}`);
-//   };
-
-//   const handleDelete = (id) => {
-//     console.log(`Delete row with id ${id}`);
-//   };
-//   return (
-//     <div></div>
-
-//     // <div style={{ height: 400, width: "100%" }}>
-//     //   <DataGrid
-//     //     rows={rows}
-//     //     columns={columns}
-//     //     initialState={{
-//     //       pagination: {
-//     //         paginationModel: { page: 0, pageSize: 5 },
-//     //       },
-//     //     }}
-//     //     pageSizeOptions={[5, 10]}
-//     //     checkboxSelection
-//     //   />
-//     // </div>
-//   );
-// };
-
-// export default AssetsList;
